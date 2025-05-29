9334ddbfb14a59448673dc4856fa4e2cccbbe014
FrancescoMer <mercoglianofrancesco10@gmail.com>
2025-05-29 16:10:11 +0200

    end

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -39,8 +39,19 @@ console.log(teachers)
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
-const lewisIndex = null;
+let lewisIndex = null;
+for (let i = 0; i < teachers.length; i++) {
+  if (teachers[i] === 'Lewis') {
+    lewisIndex = i;
+    
+  }
+}
+console.log(lewisIndex);
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = null;
+let isTeachersEmpty = true;
+for (let i = 0; i < teachers.length; i++) {
+  isTeachersEmpty = false;
+}
+console.log(isTeachersEmpty);
