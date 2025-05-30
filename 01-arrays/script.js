d5150fcce6a6325a8ba36bf0d8a10ac5aa75b13a
FrancescoMer <mercoglianofrancesco10@gmail.com>
2025-05-30 09:36:48 +0200

    bonus

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -56,3 +56,49 @@ for (let i = 0; i < teachers.length; i++) {
   break;
 }
 console.log(isTeachersEmpty); 
+
+
+// 1. Inverti l'ordine degli insegnanti nell'array teachers
+// e salva il risultato nella variabile reversedTeachers
+
+let reversedTeachers = [];
+
+for (let i = teachers.length - 1; i >= 0; i--) {
+  reversedTeachers.push(teachers[i]);
+}
+
+console.log(reversedTeachers);
+
+// 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
+// con un nome di lunghezza maggiore o uguale a 5 caratteri
+
+let longNames = [];
+
+for (let i = 0; i < teachers.length; i++) {
+  if (teachers[i].length >= 5) {
+    longNames.push(teachers[i]);
+  }
+}
+
+console.log(longNames); 
+
+// 3. Rimuovi 'Ed' dall'array teachers
+
+let updatedTeachers = [];
+for (let i = 0; i < teachers.length; i++) {
+  if (teachers[i] !== 'Ed') {
+    updatedTeachers.push(teachers[i]);
+  }
+}
+console.log(updatedTeachers); 
+
+// 4. Verifica se 'Fabio' è presente nell'array teachers
+// e salva il risultato nella variabile isFabioPresent
+let isFabioPresent = false;
+for (let i = 0; i < teachers.length; i++) {
+  if (teachers[i] === 'Fabio') {
+    isFabioPresent = true;
+    break;
+  }
+}
+console.log(isFabioPresent);
